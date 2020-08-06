863063d55ea6ede416a5ba9da5cd16a0282fc8d0
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-08-06 18:55:03 -0500

    boiler code

---
+++ src/main/java/com/squarehelpjavareact/squarehelpjavareact/SquarehelpJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.squarehelpjavareact.squarehelpjavareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquarehelpJavaReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquarehelpJavaReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/squarehelpjavareact/squarehelpjavareact/SquarehelpJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squarehelpjavareact.squarehelpjavareact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquarehelpJavaReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
